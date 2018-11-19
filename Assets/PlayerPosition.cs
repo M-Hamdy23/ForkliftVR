ecac1cf687f718fe5b8a7644eb2a1e07ce8d41e1
= <moh_ussama@hotmail.com>
2018-11-19 05:54:50 +0200

    setting right position for fixing and driving

---
+++ Assets/PlayerPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour {
+
+    public Transform player;
+    public Vector3Variable fixingTirePosition;
+    public Vector3Variable drivingPoasition;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTireFix()
+    {
+        player.position = fixingTirePosition.value;
+    }
+    public void OnDriving()
+    {
+        player.position = drivingPoasition.value;
+    }
+}
---
+++ Assets/Scripts/DamagedTire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagedTire : MonoBehaviour {
+
+    public BoolVariable damagedTire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (damagedTire.value=true)
+        {
+            //if trigger call bumpTire
+        }
+	}
+    public void BumpTire()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        damagedTire.value = false;
+    }
+    public void FlatTire()
+    {
+        transform.localScale = new Vector3(1, 1, .15f);
+        damagedTire.value = true;
+    }
+}
