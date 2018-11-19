669932b53dd5df58cbd1f6315167527f867ed240
MohamedHamdy <Mohamedhamdy231995@gmail.com>
2018-11-19 06:58:25 +0200

    LiftStickMapping

---
+++ Assets/Scripts/LiftingStickMapping.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftingStickMapping : MonoBehaviour {
+
+    public FloatVariable LiftingStick;
+    public float value;
+    public GameObject stick;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Mapping()
+    {
+        LiftingStick.value = value;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.name== stick.name)
+        {
+            Mapping();
+            print("map= " + value);
+        }
+    }
+}
